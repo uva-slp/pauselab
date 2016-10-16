9675719e6defa401c939d4a2e575c02ce13ec561
mtfalls <goldjet45@gmail.com>
2016-10-16 14:38:49 -0400

    added website link to proposals table

--- app/app/models/idea.rb
+++ app/app/models/idea.rb
@@ -1,3 +1,8 @@
 class Idea < ApplicationRecord
+	validates_presence_of :first_name, :on => :create
+	validates_presence_of :last_name, :on => :create
+	validates_presence_of :phone, :on => :create
+	validates_presence_of :description, :on => :create
 
+	scope :status, -> (status) { where status: status }
 end
--- app/db/schema.rb
+++ app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161015203447) do
+ActiveRecord::Schema.define(version: 20161016183502) do
 
   create_table "categories", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name",       null: false
@@ -38,11 +38,12 @@ ActiveRecord::Schema.define(version: 20161015203447) do
 
   create_table "proposals", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer  "cost"
-    t.text     "description", limit: 65535
+    t.text     "description",  limit: 65535
     t.string   "status"
-    t.text     "essay",       limit: 65535
-    t.datetime "created_at",                null: false
-    t.datetime "updated_at",                null: false
+    t.text     "essay",        limit: 65535
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.string   "website_link"
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
