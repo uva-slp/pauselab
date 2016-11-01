67d22c6ba643d7800564bdc0a90e0972f2687ee5
ag5ct <ag5ct@virginia.edu>
2016-10-31 23:40:40 -0400

    Github hates me. Added test case files.

--- app/test/models/user_test.rb
+++ app/test/models/user_test.rb
@@ -1,7 +1,45 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+	# @SUGAT: Corresponding test case
+	# test "user: valid" do
+	# 	user = users(:valid_human)
+	# 	assert user.save, "user not saved: missing none"
+	# 	#assert: should happen
+	# 	#"" After means it didn't happen
+	# end
+
+	test "user: missing first name" do
+		user = users(:missing_first_name)
+		assert_not user.save, "user saved: missing first name"
+	end
+
+	test "user: missing last name" do
+		user = users("missing_last_name")
+		assert_not user.save, "user saved: missing last name"
+	end
+
+	test "user: missing password" do
+		user = users(:missing_password)
+		assert_not user.save, "user saved: missing password"
+		#assert_not: should not happen
+		#"" After means it did happen (which is messed up)
+   	end
+
+   	test "user: missing email" do
+   		user = users(:missing_email)
+   		assert_not user.save , "user saved: missing email"
+   	end
+
+   	test "user: missing phone" do
+   		user = users(:missing_phone)
+   		assert_not user.save, "user saved: missing phone number"
+   	end
+
+   	# MUST FIX LATER
+   	# test "user: invalid phone" do
+   	# 	user = users(:invalid_phone)
+   	# 	assert_not user.save, "user saved: invalid phone number"
+   	# end
+
 end
