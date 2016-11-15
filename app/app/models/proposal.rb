0ff2e0667df5691e60471c8e8b186146ce18c405
hmg8je <hmg8je@virginia.edu>
2016-11-15 00:18:47 -0500

    Fixed failed test

--- app/app/models/proposal.rb
+++ app/app/models/proposal.rb
@@ -1,6 +1,6 @@
 class Proposal < ApplicationRecord
     belongs_to :user
-    has_many :vote
+    has_many :vote, :dependent => :delete_all
     has_attached_file :artist_cv
     validates_attachment :artist_cv, content_type: { content_type: ['application/pdf'] }
     #commit
