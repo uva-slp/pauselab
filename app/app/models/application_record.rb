6b8fab32cc38729a4cf59b5c4e4fcbd6bf875ed9
ag5ct <ag5ct@virginia.edu>
2016-11-15 04:25:53 -0500

    Removed testing error. Need to sanitize wysiwyg contents

--- app/app/models/application_record.rb
+++ app/app/models/application_record.rb
@@ -1,10 +1,3 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
-
-  require 'sanitize'
-  before_save :sanitize_description
-
-  def sanitize_description
-  	Sanitize.fragment(description, Sanitize::Config::RELAXED)
-  end
 end
--- app/app/models/proposal.rb
+++ app/app/models/proposal.rb
@@ -4,11 +4,4 @@ class Proposal < ApplicationRecord
     validates_attachment :artist_cv, content_type: { content_type: ['application/pdf'] }
     #commit
     validates :cost, :essay, :description, :artist_fees, :project_materials, :printing, :marketing, :documentation, :volunteer, :insurance, :events, presence:true
-
-    require 'sanitize'
-  	before_save :sanitize_description
-
-  	def sanitize_description
-  		Sanitize.fragment(description, Sanitize::Config::RELAXED)
-  	end
 end
