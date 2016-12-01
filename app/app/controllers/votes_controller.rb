303753fd1ecc4cb3aa5bb0b62361888da745c580
mtfalls <goldjet45@gmail.com>
2016-12-01 11:34:46 -0500

    cookie set after voting

--- app/app/controllers/votes_controller.rb
+++ app/app/controllers/votes_controller.rb
@@ -8,12 +8,17 @@ class VotesController < ApplicationController
   def new
     @vote = Vote.new
     @proposals = Proposal.where(status: :unchecked)
+    if cookies[:voted] != nil
+      flash[:notice] = 'You have already voted'
+      redirect_to root_path
+    end
   end
 
   def create
     @vote = Vote.new(vote_params)
     if @vote.save
-      flash[:notice] = 'You just voted'
+      flash[:notice] = 'Your vote has been received!'
+      cookies[:voted] = { :value => "already voted", :expires => Time.now + 2628000 }
       redirect_to root_path
     else
       render 'new'
