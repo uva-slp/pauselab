2ea9c96b61b2aa3a77cf673dab0715ec3f95c584
hmg8je <hmg8je@virginia.edu>
2016-12-12 14:00:46 -0500

    Voting unit tests

--- app/test/controllers/votes_controller_test.rb
+++ app/test/controllers/votes_controller_test.rb
@@ -4,4 +4,32 @@ class VotesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+   setup do
+     @proposal1 = proposals(:two) # valid fixture
+     @proposal2 = proposals(:three) # valid fixture
+     @proposal3 = proposals(:approved) # valid fixture
+     @proposal4 = proposals(:four)
+     @proposal5 = proposals(:one) #invalid fixture
+
+  end
+
+  
+   test "should get index" do
+    sign_in_as :resident
+    get votes_url
+    assert_response :success
+   end
+
+   test "Create a vote with valid proposals" do
+    sign_in_as :resident
+    assert_difference('Vote.count', 1) do
+      post votes_url, params: {
+        vote: {
+          proposal_ids:[@proposal1.id, @proposal2.id, @proposal3.id]
+         }
+       }
+    end
+    assert_redirected_to root_path
+  end
+
 end
--- app/test/models/vote_test.rb
+++ app/test/models/vote_test.rb
@@ -1,7 +1,34 @@
 require 'test_helper'
 
 class VoteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+   setup do
+     @proposal1 = proposals(:two) # valid fixture
+     @proposal2 = proposals(:three) # valid fixture
+     @proposal3 = proposals(:approved) # valid fixture
+     @proposal4 = proposals(:four)
+     @proposal5 = proposals(:one) #invalid fixture
+
+  end
+
+  test "Should save vote" do
+    vote = Vote.new
+    vote.proposal_ids=[@proposal1.id, @proposal2.id, @proposal3.id]
+    assert vote.save, "could not save vote"
+  end
+
+   test "Shouldn't save vote without three proposals" do
+    vote = Vote.new
+    vote.proposal_ids=[@proposal1.id, @proposal2.id]
+    assert_not vote.save, "Vote saved even with only three proposals"
+  end
+
+    test "Shouldn't save vote with invalid proposal " do
+    vote = Vote.new
+    vote.proposal_ids=[@proposal1.id, @proposal2.id, @proposal5.id]
+    assert_not vote.save, "Vote saved when one proposal was not valid"
+  end
+
+   
+    
 end
