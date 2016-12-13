6f2c5a73a9a1838ec1f26413734f55f5f35f836c
mtfalls <goldjet45@gmail.com>
2016-12-13 09:29:52 -0500

    some admin unit tests

--- app/test/controllers/admins_controller_test.rb
+++ app/test/controllers/admins_controller_test.rb
@@ -8,6 +8,53 @@ class AdminsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "index should redirect resident" do
+    sign_in_as :resident
+    assert_response :redirect
+    follow_redirect!
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "index should redirect steerer" do
+    sign_in_as :steerer
+    assert_response :redirect
+    follow_redirect!
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "index should redirect artist" do
+    sign_in_as :artist
+    assert_response :redirect
+    follow_redirect!
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+  test "index should redirect moderator" do
+    sign_in_as :super_artist
+    assert_response :redirect
+    follow_redirect!
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+
+  test "index should redirect super_artist" do
+    sign_in_as :super_artist
+    assert_response :redirect
+    follow_redirect!
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
+
+
   test "should get users index" do
     sign_in_as :admin
     get list_users_path
@@ -27,12 +74,6 @@ class AdminsControllerTest < ActionDispatch::IntegrationTest
     phase.phase = Phase.phases[:ideas]
     assert_equal Phase.get_current.phase, phase.phase
   end
-
-
-  # test "should change phase" do
-  #   sign_in_as :admin
-
-
 
 
 end
