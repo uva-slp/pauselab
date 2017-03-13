f383670682b13430195bd8e8a20bab9a99640763
hmg8je <hmg8je@virginia.edu>
2017-03-13 12:44:44 -0400

    pull request

--- app/app/controllers/landingpages_controller.rb
+++ app/app/controllers/landingpages_controller.rb
@@ -21,10 +21,15 @@ class LandingpagesController < ApplicationController
   def update
     @landingpage = Landingpage.find(params[:id])
     if @landingpage.update landingpage_params
-          redirect_to root_path
+      redirect_to root_path
+      #if @landingpage.title == 'Home'
+      #  redirect_to root_path
+      #elsif @landingpage.title == 'Artist'
+      #  redirect_to artist_home_url
     else
       render 'edit'
     end
+  
   end
 
   private
