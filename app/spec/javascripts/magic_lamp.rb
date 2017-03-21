a48f03911b1b9ddd341ba2834ef535638bf21033
Sugat Poudel <sp5pe@virginia.edu>
2017-03-20 21:39:03 -0400

    fixed magic_lamp error

--- app/spec/javascripts/magic_lamp.rb
+++ app/spec/javascripts/magic_lamp.rb
@@ -8,12 +8,3 @@ MagicLamp.register_fixture controller: VotesController do
   @vote = Vote.new
   render "votes/new"
 end
-
-MagicLamp.register_fixture controller: IdeasController do
-  10.times do
-    FactoryGirl.create :idea,
-      :status => :approved
-  end
-  @ideas = Idea.all
-  render "ideas/proposal_collection"
-end
