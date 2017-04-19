ba8be7c75325be488ccc0b86a304d1afff257934
SebastianJay <jgs3cd@virginia.edu>
2017-04-18 22:53:46 -0400

    move localization to active record for better integration

--- app/app/models/application_record.rb
+++ app/app/models/application_record.rb
@@ -1,6 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
+  # credit http://stackoverflow.com/a/36335591
+  def self.human_enum_name(enum_name, enum_value)
+    I18n.t("activerecord.attributes.#{model_name.i18n_key}.#{enum_name.to_s.pluralize}.#{enum_value}")
+  end
+
   protected
   def self.gen_csv attributes
     CSV.generate(headers:true) do |csv|
