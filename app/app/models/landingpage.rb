9f36f9854a9df286efd6c9bd5a1c9f44586a2910
SebastianJay <jgs3cd@virginia.edu>
2017-04-26 23:06:48 -0400

    refactor landing pages + simple tests to cover more lines

--- app/app/models/landingpage.rb
+++ app/app/models/landingpage.rb
@@ -1,9 +1,12 @@
 class Landingpage < ApplicationRecord
-   require 'sanitize'
-   before_save :sanitize_description
-   validates :title, :description, presence: true
-   
-  	def sanitize_description
-  		Sanitize.fragment(description, Sanitize::Config::RELAXED)
-   	end
+  require 'sanitize'
+  before_save :sanitize_description
+  validates :title, :description, presence: true
+  validates :title, uniqueness: true
+
+  enum title: [:about, :artist_home, :steering_home, :ideas_home]
+
+  def sanitize_description
+    Sanitize.fragment(description, Sanitize::Config::RELAXED)
+  end
 end
