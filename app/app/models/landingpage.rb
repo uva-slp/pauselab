3cd1077e7e74a38d6fb4a6a488e0c5b5a4eaa8ff
SebastianJay <jgs3cd@virginia.edu>
2017-05-06 01:18:12 -0400

    add more landing pages

--- app/app/models/landingpage.rb
+++ app/app/models/landingpage.rb
@@ -4,7 +4,7 @@ class Landingpage < ApplicationRecord
   validates :title, :description, presence: true
   validates :title, uniqueness: true
 
-  enum title: [:about, :artist_home, :steering_home, :ideas_home]
+  enum title: [:about, :artist_home, :steering_home, :ideas_home, :calling_artists, :voting_instructions, :proposal_instructions]
 
   def sanitize_description
     Sanitize.fragment(description, Sanitize::Config::RELAXED)
