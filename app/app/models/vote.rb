35713c2b451387d1b49e707bcf4d5290901ce302
SebastianJay <jgs3cd@virginia.edu>
2017-05-09 00:53:02 -0400

    vote field validation

--- app/app/models/vote.rb
+++ app/app/models/vote.rb
@@ -2,8 +2,9 @@ class Vote < ApplicationRecord
   has_and_belongs_to_many :proposals
   belongs_to :iteration
 
-  validates :phone, allow_blank: true, length: { is: 10 }
-  validates :email, allow_blank: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
+  validates :first_name, :last_name, :phone, :email, :street, :city, :state, :zip_code, presence: true
+  validates :phone, length: { is: 10 }
+  validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }
 
   # only keep numbers in phone before validation. credit http://stackoverflow.com/a/28621417
 	before_validation :format_phone_number
